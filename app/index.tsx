f23b0bcfe0101bb14d3908e803a913895788c0bc
Ben Commet <bencommet@gmail.com>
2026-04-08 15:05:09 -0500

    added in text inputs and buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,23 @@
+import { router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function HomeScreen() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function handleSubmit(){
+    router.replace('/loginSuccessScreen')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Welcome to Expo Router!</Text>
+      <TextInput  placeholder='Username' onChangeText={setUsername} value={username}/>
+      <TextInput  placeholder='Password' onChangeText={setPassword} value={password}/>
+      <Button title='submit' onPress={handleSubmit}/>
       <StatusBar style="auto" />
     </View>
   );
